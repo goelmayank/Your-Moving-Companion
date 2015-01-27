dbf6e4330f63903a8e08fa4c841ff5569d00bed4
Mayank Goyal <f2014768@pilani.bits-pilan.ac.in>
2015-01-28 00:11:44 +0530

    Revert "Revert "Update script.js""
    This reverts commit c1f8953ea5fe756fc1b3f7c0ce7c6e42ff3600a4.

--- script.js
+++ script.js
@@ -11,8 +11,20 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    
     // YOUR CODE GOES HERE!
 
     return false;
